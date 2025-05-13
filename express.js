58fb6bb9ee8a80c509fa4f378f9aedb8f86e4d0e
ruanjian230136 <1477661761@qq.com>
2025-05-13 10:40:33 +0800

    创建express.js文件
    express框架

---
+++ express.js
@@ -0,0 +1,13 @@
+const express=require('express');
+//创建web服务器实例对象
+const app=express();
+
+//创建请求地址为‘/’的get请求方式的路由
+app.get('/',(req,res)=>{
+    res.send('<h1>你好，Hello World</h1>');
+});
+// 监听端口3000
+app.listen(3000,()=>{
+    console.log('服务器已启动');
+});
+
